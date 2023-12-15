26b543010d3433edb046ebf70bcaf202b567a22b
teuncm <teun.c.mathijssen@gmail.com>
2023-12-15 13:20:19 +0100

    Changed docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,18 +16,9 @@ release = 'nowww'
 
 extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
-templates_path = []
-exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
 
 html_theme = 'sphinx_rtd_theme'
-
-html_static_path = []
-
-html_output_folder=['html']
