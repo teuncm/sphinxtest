aa23683be39bdaecb45eb84998705d84e1250960
teuncm <teun.c.mathijssen@gmail.com>
2023-12-15 12:01:47 +0100

    Add sources

---
+++ build/_sources/sphinxtest.rst.txt
@@ -0,0 +1,21 @@
+sphinxtest package
+==================
+
+Submodules
+----------
+
+sphinxtest.sphinxtest module
+----------------------------
+
+.. automodule:: sphinxtest.sphinxtest
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: sphinxtest
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ src/sphinxtest/sphinxtest.py
@@ -0,0 +1,7 @@
+def a(b):
+    """Add 1 to b and return it."""
+    return b + 1
+
+def c(a=3, b=5):
+    """Always return True! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra laoreet rhoncus. Donec tincidunt eget ligula et hendrerit. Nullam pharetra nisi nec turpis rhoncus, ac lacinia nulla ultricies. Morbi in venenatis felis, eget ornare mi. Nulla facilisi. Vestibulum id efficitur nulla. Suspendisse a lacus in ante interdum efficitur. Nunc arcu velit, luctus quis vulputate quis, dapibus non neque. """
+    return True
